478ed0e2cae931cb8ed9bf4450e6f0f5ef0c1ea1
Murilo Feres <muriloferes90@gmail.com>
2020-08-06 10:02:25 -0300

    ...

--- javascript/view/painel.js
+++ javascript/view/painel.js
@@ -135,6 +135,15 @@ function montar_grafico(id, chartData){
 			drawLine((listSum(positivos) / positivos.length), 'rgba(0, 200, 0, 1)', 1);
 			drawLine((listSum(negativos) / negativos.length), 'rgba(255, 0, 0, 1)', 1);
 			drawLine((listSum(final) / final.length), 'rgba(255, 255, 0, 1)', 3);
+
+			const ctx = chartInstance.chart.ctx;
+			ctx.textAlign = 'right';
+			ctx.textBaseline = 'top';
+			ctx.font = Chart.helpers.fontString(10, 'normal', Chart.defaults.global.defaultFontFamily);
+			ctx.fillStyle = 'rgba(0, 100, 0, 1)';
+			ctx.fillText(`Positivos: ${positivos.length} (${Math.round(positivos.length / final.length * 100)}%)`, chartInstance.chartArea.right, 8);
+			ctx.fillStyle = 'rgba(150, 0, 0, 1)';
+			ctx.fillText(`Negativos: ${negativos.length} (${Math.round(negativos.length / final.length * 100)}%)`, chartInstance.chartArea.right, 20);
 		}
 	}];
 
